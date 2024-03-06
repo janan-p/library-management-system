4765eabfa3fd6c8d3f3aded9e858044e2e49190b
Viral Bhavsar <viralkum@ualberta.ca>
2024-03-05 22:23:18 -0700

    Took a step

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import sqlite3
 
+#print ("Hi")
 connection = None
 cursor = None
 
 def connect(path):
+    global connection, cursor
+
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+    cursor.execute(' PRAGMA foreign_keys=ON; ')
+    connection.commit()
+    return
+
+def insert_data():
     pass
 
-
 def main():
-    global connection, cursor
+    '''Main function to run the code'''
     
+    global connection, cursor
+    path = input("Enter the database file name: ")
+    path = './' + path
+
+    connect(path)
+
+
+    connection.commit()
+    connection.close()
+    return
+    
+
+if __name__ == "__main__":
+    main()
