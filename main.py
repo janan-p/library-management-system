ab98279fd60f68b344e6c1280bc7e689f6f79b08
Viral Bhavsar <viralkum@ualberta.ca>
2024-03-05 23:15:36 -0700

    Working on the login section

--- main.py
+++ main.py
@@ -13,18 +13,38 @@ def connect(path):
     connection.commit()
     return
 
+def login(): #logs the user in or signs them up
+    global connection, cursor
+
+    option_choosen = True
+    user_id = ''
+
+    while option_choosen == True:
+        user_option = input("\nDo you have an account? (Yes or no) \n If you want to exit (exit).\n")
+
+        if user_option.lower() == "exit":#exiting the code
+            quit()
+
+        if user_option.lower() == "yes" or user_option.lower() == "y": #User already has account, then sign them in
+            pass
+        elif user_option.lower() == "no" or user_option.lower() == "n": #User does not have account, sign them up
+            pass
+        else:
+            print("\nInvalid input! Type either 'Yes' or 'No'\n")
+
+
 def insert_data():
     pass
 
 def main():
     '''Main function to run the code'''
-    
+
     global connection, cursor
     path = input("Enter the database file name: ")
     path = './' + path
 
     connect(path)
-
+    login()
 
     connection.commit()
     connection.close()
