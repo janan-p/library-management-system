ae8d67c059ccb09c9fc66be8521ae278f4a44ced
dhanshri205 <156151265+dhanshri205@users.noreply.github.com>
2024-03-14 10:38:16 -0600

    Update database_creation.sql

--- database_creation.sql
+++ database_creation.sql
@@ -55,48 +55,67 @@ CREATE TABLE reviews(
     FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
 
-INSERT INTO members values('jpanchal@ualberta.ca', 'bestpersonalive', 'Janan', 2003, 'CS');
-INSERT INTO members values('asshah1@ualberta.ca','annoyingiuc123', 'IUC', 1980, 'Physics');
-INSERT INTO members values('viralkum@ualberta.ca', 'new-haircut', 'Vasulevskiy', 2004, 'English');
-INSERT INTO members values('dhanshri@ualberta.ca', 'alwaysEXCITED', 'Dhanshri', 1990, 'CS');
-INSERT INTO members values('arch@ualberta.ca', 'i_can_cook', 'Archi', 2005, 'Engineering');
-INSERT INTO members values('siddh@ualberta.ca', 'TaLl', 'Siddhant', 1980, 'Science');
-INSERT INTO members values('annoying@ualberta.ca', 'monkey_00', 'Om', 2005, 'Business');
-INSERT INTO members values('dhiya@ualberta.ca', 'Cutie123', 'Dhiya', 1990, 'English');
-INSERT INTO members values('keya@ualberta.ca', 'VasusJustFriend', 'Keya', 2003, 'Science');
+Books Table
+1|Book 1|Vasulevskiy|2002
+2|Book 2|Robert|2022
+3|Book 3|Rowling|2024
+4|Book 4|Arthur|2020
+5|Book 5|Arash|2017
+6|Book 6|Rejwana|2017
+7|Book 7|Archii|2020
+8|Book 8|Dhanshri|2019
+9|Book 9|Vasu|2069
+10|Book 10|Arham|2018
 
-INSERT INTO books values(1, 'Book 1', 'Vasulevskiy', 2002);
-INSERT INTO books values(2, 'Book 2', 'Robert', 2022);
-INSERT INTO books values(3, 'Book 3', 'Rowling', 2024);
-INSERT INTO books values(4, 'Book 4', 'Arthur', 2020);
-INSERT INTO books values(5, 'Book 5', 'Arash', 2017);
-INSERT INTO books values(6, 'Book 6', 'Rejwana', 2017);
-
-
-INSERT INTO borrowings values(1, 'arch@ualberta.ca', 1, '2023-11-15', NULL);
-INSERT INTO borrowings values(2, 'arch@ualberta.ca', 1, '2023-11-15', NULL);
-INSERT INTO borrowings values(3, 'siddh@ualberta.ca', 2, '2023-11-15', NULL);
-INSERT INTO borrowings values(4, 'keya@ualberta.ca', 2, '2023-10-15', '2023-10-25');
-INSERT INTO borrowings values(5, 'keya@ualberta.ca', 3, '2023-10-15', '2023-10-25');
-INSERT INTO borrowings values(6, 'dhiya@ualberta.ca', 3, '2023-10-15', '2023-11-25');
-INSERT INTO borrowings values(7, 'annoying@ualberta.ca', 3, '2023-10-15', '2023-11-25');
-INSERT INTO borrowings values(8, 'dhanshri@ualberta.ca', 3, '2023-10-15', '2023-10-25');
-INSERT INTO borrowings values(9, 'jpanchal@ualberta.ca', 4, '2023-11-15', NULL);
-INSERT INTO borrowings values(10, 'asshah1@ualberta.ca', 4, '2023-11-15', NULL);
-INSERT INTO borrowings values(11, 'viralkum@ualberta.ca', 4, '2023-11-15', NULL);
-INSERT INTO borrowings values(12, 'jpanchal@ualberta.ca', 5, '2023-11-15', NULL);
-
-INSERT INTO penalties values(1, 1, 50, NULL);
-INSERT INTO penalties values(2, 2, 50, 20);
-INSERT INTO penalties values(3, 1, 50, 50);
-INSERT INTO penalties values(4, 3, 60, 60);
-INSERT INTO penalties values(5, 5, 90, 70);
-INSERT INTO penalties values(6, 10, 50, NULL);
-INSERT INTO penalties values(7, 12, 70, 70);
-
-INSERT INTO reviews values(1, 2, 'siddh@ualberta.ca', 4, 'Amazing book','2023-12-15');
-INSERT INTO reviews values(2, 2, 'keya@ualberta.ca', 3, 'Good storyline','2023-12-20');
-INSERT INTO reviews values(3, 3, 'annoying@ualberta.ca', 1, 'I hated this book','2023-12-30');
-INSERT INTO reviews values(4, 1, 'arch@ualberta.ca', 2, 'Good detail','2023-12-31');
-INSERT INTO reviews values(5, 4, 'asshah1@ualberta.ca', 1, 'Hate reading','2024-01-19');
-INSERT INTO reviews values(6, 5, 'jpanchal@ualberta.ca', 4, 'My favourite book','2023-02-23');
+Borrowings Table
+1|arch@ualberta.ca|1|2023-11-15|
+2|arch@ualberta.ca|2|2023-11-15|
+3|siddh@ualberta.ca|3|2023-11-15|
+4|keya@ualberta.ca|4|2023-10-15|2023-10-25
+5|keya@ualberta.ca|5|2023-10-15|2023-10-25
+6|dhiya@ualberta.ca|6|2023-10-15|2023-11-25
+7|annoying@ualberta.ca|4|2023-10-15|2023-11-25
+8|dhanshri@ualberta.ca|5|2023-10-15|2023-10-25
+9|jpanchal@ualberta.ca|6|2023-11-15|
+10|asshah1@ualberta.ca|7|2023-11-15|
+    
+Members Table
+jpanchal@ualberta.ca|bestpersonalive|Janan|2003|CS
+asshah1@ualberta.ca|annoyingiuc123|IUC|1980|Physics
+viralkum@ualberta.ca|new-haircut|Vasulevskiy|2004|English
+dhanshri@ualberta.ca|alwaysEXCITED|Dhanshri|1990|CS
+arch@ualberta.ca|i_can_cook|Archi|2005|Engineering
+siddh@ualberta.ca|TaLl|Siddhant|1980|Science
+annoying@ualberta.ca|monkey_00|Om|2005|Business
+dhiya@ualberta.ca|Cutie123|Dhiya|1990|English
+keya@ualberta.ca|VasusJustFriend|Keya|2003|Science
+jpanc@ualberta.ca|janan|jan|2004|CS
+afhlfjl@ualberta.ca|faltu|test1|2003|Business
+iuc@ualberta.ca|Hello|Aayushi|2005|Paleontoloy
+hi@gmail.com|hi|hi|2005|cs
+    
+Penalties Table
+1|1|10|
+2|2|50|50
+3|1|100|50
+4|3|60|60
+5|5|90|70
+6|10|50|
+7|12|70|70
+8|4|50|20
+9|4|50|20
+10|6|100|
+11|7|30|10
+12|8|150|150
+13|9|20|4
+14|10|50|10
+15|11|800|200
+16|12|30|15
+    
+Reviews Table
+1|2|siddh@ualberta.ca|4|Amazing book|2023-12-15
+2|2|keya@ualberta.ca|3|Good storyline|2023-12-20
+3|3|annoying@ualberta.ca|1|Yo that shit was ass|2023-12-30
+4|1|arch@ualberta.ca|2|Good detail|2023-12-31
+5|4|asshah1@ualberta.ca|1|Hate reading|2024-01-19
+6|5|jpanchal@ualberta.ca|4|My favourite book|2023-02-23
